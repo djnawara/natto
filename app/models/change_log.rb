eb49ae56fbb0d1bab11fa698eab71ba3d354bb92
David Nawara <djnawara@gmail.com>
2008-07-09 17:51:13 -0500

    Validates length of comments only if not blank.

--- app/models/change_log.rb
+++ app/models/change_log.rb
@@ -1,10 +1,15 @@
 class ChangeLog < ActiveRecord::Base
   validates_presence_of :comments
-  validates_length_of :comments, :within => 10..1000
+  validates_length_of :comments, :within => 10..1000, :if => :comments_not_blank?
   
   belongs_to :user
   
   def self.get_activity_for_range(start, finish, object_class = '%', object_id = '%', action_name = '%')
     ChangeLog.count_by_sql("SELECT COUNT(id) FROM change_logs WHERE object_class LIKE '#{object_class}' AND object_id LIKE '#{object_id}' AND action LIKE '#{action_name}' AND performed_at > '#{start}' AND performed_at < '#{finish}'")
   end
+  
+  def comments_not_blank?
+    !comments.blank?
+  end
+  private :comments_not_blank?
 end
