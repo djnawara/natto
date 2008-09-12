db6201becdf27de26e184cffd3fc1037e2b947e5
David Nawara <djnawara@gmail.com>
2008-09-12 12:10:59 -1000

    Renamed the file.

--- config/initializers/natto_inflections.rb
+++ config/initializers/natto_inflections.rb
@@ -1,4 +1,7 @@
 Inflector.inflections do |inflect|
   inflect.irregular 'medium', 'media'
+  inflect.plural    /(ia)$/i, '\1'
+  inflect.plural    /(ium)$/i, 'ia'
+  inflect.singular  /(ia)$/i, 'ium'
   #inflect.irregular 'inventory', 'inventories'
 end
