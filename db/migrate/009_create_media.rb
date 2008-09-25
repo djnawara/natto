85095b3f225042a1f57bdc8dee69a943119061f4
David Nawara <djnawara@gmail.com>
2008-09-25 17:38:54 -0500

    Added attachment_fu required columns.

--- db/migrate/009_create_media.rb
+++ db/migrate/009_create_media.rb
@@ -1,8 +1,16 @@
 class CreateMedia < ActiveRecord::Migration
   def self.up
     create_table :media do |t|
-      t.string        :location
-      t.string        :type
+      t.string        :title
+      t.text          :description
+      t.integer       :parent_id
+      t.string        :content_type
+      t.string        :filename
+      t.string        :thumbnail
+      t.integer       :size
+      t.integer       :width
+      t.integer       :height
+      t.string        :medium_type
       t.string        :state
     end
   end
