fb3e68aec8ce70196f2005ff9040cb52f8511fb1
David Nawara <djnawara@gmail.com>
2008-09-25 17:29:56 -0500

    Bug fix; converted to multi-part.

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -1,22 +1,36 @@
 class UserMailer < ActionMailer::Base
   def signup_notification(user)
+    # setup all of our variables
     setup_email(user)
-    @subject    += 'Please activate your new account'
-    @body[:url]  = "http://#{Djn2ms.host}/activate/#{user.activation_code}"
+    @subject       += 'Please activate your new account'
+    variables_hash  = {:user => user, :url => "http://#{Natto.host}/activate/#{user.activation_code}"}
+    # build the email parts
+    part( :content_type => "multipart/alternative" ) do |p|
+      p.part(:content_type => "text/plain",
+             :body => render_message("signup_notification.text.plain.haml", variables_hash))
+      p.part(:content_type => "text/html",
+             :body => render_message("signup_notification.text.html.haml", variables_hash))
+    end
   end
   
   def forgetful_notification(user)
     setup_email(user)
-    @subject    += 'Password reset request'
-    @body[:url]  = "http://#{Djn2ms.host}/reset_password/#{user.password_reset_code}"
+    @subject       += 'Password reset request'
+    variables_hash  = {:user => user, :url => "http://#{Natto.host}/reset_password/#{user.password_reset_code}"}
+    # build the email parts
+    part( :content_type => "multipart/alternative" ) do |p|
+      p.part(:content_type => "text/plain",
+             :body => render_message("forgetful_notification.text.plain.haml", variables_hash))
+      p.part(:content_type => "text/html",
+             :body => render_message("forgetful_notification.text.html.haml", variables_hash))
+   end
   end
   
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
       @from        = "#{DEFAULT_FROM}"
-      @subject     = "[#{Djn2ms.site_title}] "
+      @subject     = "[#{Natto.site_title}] "
       @sent_on     = Time.now
-      @body[:user] = user
     end
 end
