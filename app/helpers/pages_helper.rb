f6a1776d5d0b4bd5ab0815a411f19fb48dae4372
David Nawara <djnawara@gmail.com>
2008-10-02 11:42:44 -0500

    Type bug fix.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -10,7 +10,7 @@ module PagesHelper
         (page.advanced_path.blank? ? show_path(page.title.gsub(/ /, '_')) : @controller.send(page.advanced_path)),
         options.merge({:title => page.description})
       )
-    rescue Exception => err
+    rescue Exception => exception
       coder = HTMLEntities.new
       flash.now[:error] = "<h2>Advanced Path Error</h2><p>#{coder.encode(exception.message)}</p>"
     end
