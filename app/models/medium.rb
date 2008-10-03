2ad0bde8caf457b2dd32f7268338cbe5634cc234
David Nawara <djnawara@gmail.com>
2008-10-03 14:21:25 -0500

    Overrode attachment_fu functions to put thumbs into own directory.

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -37,4 +37,19 @@ class Medium < ActiveRecord::Base
     mime_types = { ".gif" => "image/gif", ".ief" => "image/ief", ".jpe" => "image/jpeg", ".jpeg" => "image/jpeg", ".jpg" => "image/jpeg", ".pbm" => "image/x-portable-bitmap", ".pgm" => "image/x-portable-graymap", ".png" => "image/png", ".pnm" => "image/x-portable-anymap", ".ppm" => "image/x-portable-pixmap", ".ras" => "image/cmu-raster", ".rgb" => "image/x-rgb", ".tif" => "image/tiff", ".tiff" => "image/tiff", ".xbm" => "image/x-xbitmap", ".xpm" => "image/x-xpixmap", ".xwd" => "image/x-xwindowdump" }
     mime_types[File.extname(file)]
   end
+  
+  # overridden to store thumbs in their own directory
+  def full_filename(thumbnail = nil)
+    file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
+    if thumbnail.blank? && self.thumbnail.blank?
+      File.join(RAILS_ROOT, file_system_path, *partitioned_path(thumbnail_name_for(thumbnail)))
+    else
+      File.join(RAILS_ROOT, file_system_path, (thumbnail.blank? ? self.thumbnail.to_s : thumbnail.to_s), *partitioned_path(thumbnail_name_for(thumbnail)))
+    end
+  end
+  
+  # Gets the thumbnail name for a filename.  'foo.jpg' becomes 'foo_thumbnail.jpg'
+  def thumbnail_name_for(thumbnail = nil)
+    return filename
+  end
 end
