03c5c40d087e4e49cbd238420a8aa533f1c8ea66
David Nawara <djnawara@gmail.com>
2008-10-13 14:04:27 -0500

    Added field for max projects to show in portfolio.

--- lib/natto.rb
+++ lib/natto.rb
@@ -1,17 +1,20 @@
 # Djn2ms module for configuration settings.
 module Natto
-  mattr_accessor :site_title            # Used in the title tag.
-  mattr_accessor :host                  # Most notably, used by the authentication system.
-  mattr_accessor :tango_home            # For locating tango icons in the application helper.
-  mattr_accessor :max_violation_votes   # The maximum number of votes on a comment violation before it's hidden.
-  mattr_accessor :small_image_size      # the size for thumbnails
-  mattr_accessor :medium_image_size     # medium resize for attachment_fu
-  mattr_accessor :large_image_size      # big stuff!
-  
+  mattr_accessor :site_title              # Used in the title tag.
+  mattr_accessor :host                    # Most notably, used by the authentication system.
+  mattr_accessor :tango_home              # For locating tango icons in the application helper.
+  mattr_accessor :max_violation_votes     # The maximum number of votes on a comment violation before it's hidden.
+  mattr_accessor :small_image_size        # the size for thumbnails
+  mattr_accessor :medium_image_size       # medium resize for attachment_fu
+  mattr_accessor :large_image_size        # big stuff!
+  mattr_accessor :portoflio_projects_max  # Max projects to display on portfolio pages
+
   self.site_title               = "Natto CMS"
   self.host                     = "localhost:3000"
   self.tango_home               = "/plugin_assets/natto/images/tango-icon-theme/"
   self.max_violation_votes      = 3
+  self.portoflio_projects_max   = 15
+
   # image sizing
   self.small_image_size         = '80x80>'
   self.medium_image_size        = '300x'
