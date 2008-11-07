0318ee6242211cc16d928039e9fa2fda2510ef6a
David Nawara <djnawara@gmail.com>
2008-11-07 14:39:41 -0600

    Updated image sizes and added width and height for formats.

--- lib/natto.rb
+++ lib/natto.rb
@@ -5,8 +5,14 @@ module Natto
   mattr_accessor :tango_home              # For locating tango icons in the application helper.
   mattr_accessor :max_violation_votes     # The maximum number of votes on a comment violation before it's hidden.
   mattr_accessor :small_image_size        # the size for thumbnails
+    mattr_accessor :small_image_width
+    mattr_accessor :small_image_height
   mattr_accessor :medium_image_size       # medium resize for attachment_fu
+    mattr_accessor :medium_image_width
+    mattr_accessor :medium_image_height
   mattr_accessor :large_image_size        # big stuff!
+    mattr_accessor :large_image_width
+    mattr_accessor :large_image_height
   mattr_accessor :portoflio_projects_max  # Max projects to display on portfolio pages
 
   self.site_title               = "Natto CMS"
@@ -16,7 +22,15 @@ module Natto
   self.portoflio_projects_max   = 15
 
   # image sizing
-  self.small_image_size         = '80x80>'
-  self.medium_image_size        = '300x'
-  self.large_image_size         = '700x700>'
+  self.small_image_width        = '80'
+  self.small_image_height       = '80'
+  self.small_image_size         = "#{self.small_image_width}x#{self.small_image_height}!"
+  
+  self.medium_image_width       = '300'
+  self.medium_image_height      = ''
+  self.medium_image_size        = "#{self.medium_image_width}x#{self.medium_image_height}"
+  
+  self.large_image_width        = '700'
+  self.large_image_height       = '700'
+  self.large_image_size         = "#{self.large_image_width}x#{self.large_image_height}>"
 end
