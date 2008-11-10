2278a8d5b3cc44cce122baee005867764181aeba
David Nawara <djnawara@gmail.com>
2008-11-10 15:59:08 -0600

    Removed obsolete method; cleaned up security and validation.

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,21 +13,10 @@ class Post < NattoBase
     MSG_TITLE_BAD   = "must not contain non-printing characters or \\&gt;&lt;&amp;/ please."
 
   validates_presence_of     :title
-  validates_length_of       :title, :within => 2..40
+  validates_length_of       :title, :within => 2..254
   validates_format_of       :title, :with => RE_TITLE_OK, :message => MSG_TITLE_BAD
 
   validates_presence_of     :content
-  validates_length_of       :content, :within => 1..9999
-
-  # Return the object's uber-parent.
-  def get_root_ancestor
-    if parent_id.nil? || parent_type.nil?
-      return self
-    else
-      parent = parent_type.constantize.find_by_id(parent_id)
-      return if parent.nil?
-      return parent if !parent.class.name.eql?(self.class.name)
-      return parent.get_root_ancestor
-    end
-  end
+  
+  attr_accessible :title, :description, :content, :page_id
 end
